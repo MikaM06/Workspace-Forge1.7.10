20eac2baf73992c092a70076356675ccf35885de
MikaM06 / Louhan <80212988+MikaM06@users.noreply.github.com>
2023-08-12 23:09:21 +0200

    fix

--- forge.bat
+++ forge.bat
@@ -7,7 +7,6 @@ echo Choisissez une option:
 
 start java -version
 
-# Vérification de l'installation de Java
 echo INSTALLE JDK 8 !!!
 
 echo 1. Eclipse (Conseiller)
@@ -19,17 +18,17 @@ if "%choix%"=="1" (
     rem Remplacez la commande ci-dessous par la commande réelle que vous souhaitez exécuter pour l'option 1
     gradlew clean setDecompWorkspace eclipse
     echo Lancement de la première commande...
-    start gradlew setupDecompWorkspace
+    gradlew setupDecompWorkspace
     echo Lancement de la 2eme commande...
-    start gradlew build
+    gradlew build
     pause
 ) else if "%choix%"=="2" (
     rem Remplacez la commande ci-dessous par la commande réelle que vous souhaitez exécuter pour l'option 2
     gradlew clean setDecompWorkspace idea
     echo Lancement de la première commande...
-    start gradlew setupDecompWorkspace
+    gradlew setupDecompWorkspace
      echo Lancement de la 2eme commande...
-     start gradlew build
+    gradlew build
     pause
 ) else (
     echo Choix invalide. Veuillez entrer 1 ou 2.
